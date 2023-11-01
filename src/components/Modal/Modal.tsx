b0cb57b10b123656d2c0ebea6b0b8b2c851e4e40
andersonaguia <andersonlaguiar@gmail.com>
2023-11-01 09:10:52 -0300

    cria componente modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+import { ModalContainer, ModalItems } from "./modal.styles";
+
+type Props = {
+  handleOpen: () => void;
+  handleClose: () => void;
+  open: boolean;
+  children: ReactNode;
+};
+
+export const CustomModal: React.FC<Props> = ({
+  handleOpen,
+  handleClose,
+  open,
+  children,
+}) => {
+  return (
+    <ModalContainer onClick={handleClose}>
+      <div>
+        <ModalItems>
+          <div>
+            <button onClick={handleClose}>&times;</button>
+          </div>
+          <div className="message">{children}</div>
+        </ModalItems>
+      </div>
+    </ModalContainer>
+  );
+};
